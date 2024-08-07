4a7be6a09b65b712140ff8abb30caa887fcbb1b1
Uday Ram <104197909+Uday124@users.noreply.github.com>
2024-08-07 12:48:36 +0530

    Add files via upload

---
+++ react-shopping-cart/README.md
@@ -0,0 +1,38 @@
+# React Shopping Cart
+
+### [Demo Link](https:///) 🔗
+
+**Shopping Cart Project** built with **React JS**, using **Context API** & **useReducer Hook**.
+
+<br/>
+
+## Features:
+
+- #### Add to Cart
+- #### Remove from Cart
+- #### Increment / Decrement the Quantity
+
+<br/>
+
+## Technologies used:
+
+- #### **React JS**
+- #### **SASS**
+
+<br/>
+
+## Author:
+
+- **Gulshan Songara** - **[Portfolio Website](https://)**, **[Linkedin](https://)**
+
+<br/>
+
+## Available Scripts:
+
+#### `npm start`
+
+#### `npm test`
+
+#### `npm run build`
+
+#### `npm run eject`
---
+++ react-shopping-cart/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://https://github.com/Uday124/react-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        github.com/Uday124/react-shopping-cart
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Uday Ram
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
