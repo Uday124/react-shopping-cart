5446b750ab179008359958378ece1361a8058933
Uday Ram <104197909+Uday124@users.noreply.github.com>
2024-08-07 21:24:49 +0530

    Update Footer.js

--- react-shopping-cart/src/components/Footer.js
+++ react-shopping-cart/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <footer id="footer">
                 <div className="container">
                     <a
-                        href="https://react-shopping-cart-info.netlify.app/"
+                        href="https://github.com/Uday124/react-shopping-cart"
                         className="repo_link"
                         target="_blank"
                         rel="noreferrer"
